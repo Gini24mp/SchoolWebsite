880be64ade59c09ff82950951b03e57504b4ff9d
Gini24mp <u21573558@tuks.co.za>
2024-01-13 17:41:04 +0200

    Add top heading and activities to arts and culture page

--- school-website/src/app/arts/page.jsx
+++ school-website/src/app/arts/page.jsx
@@ -1,10 +1,14 @@
 import NavBar from "@/components/header";
 import Footer from "@/components/footer";
+import TopHeading from "@/components/arts-and-culture/top-heading";
+import Activities from "@/components/arts-and-culture/activities";
 
 function ArtsAndCulturePage(){
     return(
         <>
             <NavBar />
+            <TopHeading />
+            <Activities />
             <Footer />
         </>
     );
